3542ea3cc9170eaf6fdd6923b6eacb4a71c9544c
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-10-10 18:06:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nFFCUDpa#Uqza7AyyxpdyKHANB2k46sDL9pOhu2PI4vj28AKLVTU",
+SESSION_ID: process.env.SESSION_ID || "rBIQTKZQ#NdDhHwvChvr1aheVq9R6m7atBzU5OiWstVxl2FLQIkg",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
