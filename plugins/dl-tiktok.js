68dcd2e388da81d12c8a0dde84cf9377449a1dd8
BLACKVENOM499 <blackvenomx499@gmail.com>
2025-10-22 14:00:00 +0530

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -16,7 +16,7 @@ async (conn, mek, m, { from, args, q, reply }) => {
         
         reply("Downloading video, please wait...");
         
-        const apiUrl = `https://delirius-apiofc.vercel.app/download/tiktok?url=https://vt.tiktok.com/ZSB2HNoKR/}`;
+        const apiUrl = `https://delirius-apiofc.vercel.app/download/tiktok?url=}`;
         const { data } = await axios.get(apiUrl);
         
         if (!data.status || !data.data) return reply("Failed to fetch TikTok video.");
